70d5b9b01a99dca172a72f6a4f1174f5cf72cf3c
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-09-16 15:27:56 +0200

    working working

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from urllib import response
+from flask import Flask, request, make_response
 import pg8000.native
 
 PASSWORD = "123"
@@ -18,3 +19,11 @@ def delete_room():
     name = request.form['name']
     db.run("DELETE FROM rooms WHERE name = (:n)", n=name)
     return "OK"
+
+@app.route("/rooms", methods = ["GET"])
+def retrieve_rooms():
+    result = []
+    for name in db.run("SELECT (name) from rooms"):
+        result.append(name)
+    res = make_response(result, 200)
+    return res
--- eco-home-front/src/routes/rooms/+page.svelte
+++ eco-home-front/src/routes/rooms/+page.svelte
@@ -1,12 +1,8 @@
 <script>
-    import Header from "./_components/Header.svelte";
+	import Header from "../_components/Header.svelte";
 </script>
 
 <Header />
-<div class="container">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis eu nulla vitae dignissim. Donec velit nisl, porta a urna ut, sodales pretium nunc. Morbi aliquam scelerisque luctus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer consectetur erat nec pharetra ultricies. Fusce scelerisque massa et justo ornare lobortis. Nam scelerisque lectus ac erat viverra, ac congue lorem dictum. Donec vitae enim dui. Suspendisse potenti. Nullam sit amet nulla vel eros accumsan vehicula at quis magna. Fusce scelerisque, sem vel euismod mollis, quam lectus aliquet augue, tincidunt consequat justo est at mauris. Curabitur a elit quis dolor cursus semper luctus ut dui. Nullam ultricies et augue non bibendum</p>
-</div>
-<!--
 <div class="columns">
     <div class="column is-one-fifth">
     
@@ -61,4 +57,3 @@
       </div>
     </div>
 </div>
--->
