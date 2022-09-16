4c215f643cb83c0c72bcf2b113287a02e384d142
SzymonGoldberg <szymon.gorka.sgorkas@gmail.com>
2022-09-16 19:40:16 +0200

    Working

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
-from urllib import response
-from flask import Flask, request, make_response
-import pg8000.native
+from flask import Flask, request, make_response # basics for building APIs
+from flask_cors import CORS # enable cross origin 
+import pg8000.native        # postgresql wrapper for python
 
 PASSWORD = "123"
 
 app = Flask(__name__)
+CORS(app)
 db = pg8000.native.Connection("postgres", password=PASSWORD)
 
 @app.route("/create_room", methods = ["POST"])
--- eco-home-front/src/routes/rooms/+page.svelte
+++ eco-home-front/src/routes/rooms/+page.svelte
@@ -1,43 +1,55 @@
 <script>
 	import Header from "../_components/Header.svelte";
+  
+  
+  const url = "http://127.0.0.1:5000";
+  let rooms_res;
+  let newRoomName = "";
+  let created_room = false;
+
+  refresh();
+
+  function refresh() {
+    rooms_res = fetch(url + "/rooms", { method : "GET"}).then(r => r.json());
+  };
+  
+  
+  async function createRoom() {
+    const response = fetch(url + "/create_room",
+    {
+      method : 'POST',
+      body: new URLSearchParams({
+        "name" : newRoomName
+      })
+    }).then(r => r.json());
+    refresh();
+  }
 </script>
 
 <Header />
 <div class="columns">
     <div class="column is-one-fifth">
-    
+      <div class="section columns is-1">
+        <input class="input is-rounded is-small column is-11" type="text" bind:value={newRoomName} placeholder="add new room" required>
+        <button class="button is-primary is-small is-rounded column" on:click={createRoom}>+</button>
+      </div>
       <aside class="menu">
-        <p class="menu-label">
-          General
-        </p>
         <ul class="menu-list">
-          <li><a>Dashboard</a></li>
-          <li><a>Customers</a></li>
-        </ul>
-        <p class="menu-label">
-          Administration
-        </p>
-        <ul class="menu-list">
-          <li><a>Team Settings</a></li>
-          <li>
-            <a class="is-active">Manage Your Team</a>
-            <ul>
-              <li><a>Members</a></li>
-              <li><a>Plugins</a></li>
-              <li><a>Add a member</a></li>
-            </ul>
-          </li>
-          <li><a>Invitations</a></li>
-          <li><a>Cloud Storage Environment Settings</a></li>
-          <li><a>Authentication</a></li>
-        </ul>
-        <p class="menu-label">
-          Transactions
-        </p>
-        <ul class="menu-list">
-          <li><a>Payments</a></li>
-          <li><a>Transfers</a></li>
-          <li><a>Balance</a></li>
+          {#await rooms_res}
+            <p>waiting for data</p>
+          {:then rooms}
+            {#if rooms.length > 0}
+              {#each rooms as room}
+                <li><a>{room}</a></li>
+              {/each}
+            {:else}
+            <li>
+              <a>There are no rooms available!</a>
+            </li>
+            {/if}
+          {:catch error}
+            <p> {error.message} </p>
+          {/await}
         </ul>
       </aside>
     
