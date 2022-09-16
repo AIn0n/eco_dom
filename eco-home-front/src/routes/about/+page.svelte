f8b6014ec0c8a3664d02b94a2e1323a79c87c188
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-09-17 00:41:53 +0200

    Early about page

--- eco-home-front/src/routes/about/+page.svelte
+++ eco-home-front/src/routes/about/+page.svelte
@@ -2,4 +2,61 @@
     import Header from "../_components/Header.svelte";
 </script>
 
+
 <Header />
+<section class="section">
+  <h1 class="title">About us</h1>
+  <h2 class="subtitle">
+    A few words about the creators of this page.
+  </h2>
+</section>
+
+
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://openailabsprodscus.blob.core.windows.net/private/user-N3lhCunQtshrWgsB36Z8Cmi5/generations/generation-k9WbteJBHJ2lKjfYkAbajFV9/image.webp?st=2022-09-16T20%3A57%3A51Z&se=2022-09-16T22%3A55%3A51Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-16T21%3A09%3A55Z&ske=2022-09-23T21%3A09%3A55Z&sks=b&skv=2021-08-06&sig=3GHewv7h6psFkVtvyPizxRxOX0wxAn0XDXYFf934p0k%3D">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Szymon Górka</strong> <small>222606</small>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+      </p>
+</article>
+
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://openailabsprodscus.blob.core.windows.net/private/user-N3lhCunQtshrWgsB36Z8Cmi5/generations/generation-EZur6EGqESMmj7mCw2SkiOZd/image.webp?st=2022-09-16T20%3A55%3A20Z&se=2022-09-16T22%3A53%3A20Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-16T21%3A06%3A40Z&ske=2022-09-23T21%3A06%3A40Z&sks=b&skv=2021-08-06&sig=m%2BQmJLJbB10G2f9MhaAzDMYEQHt9k6qiMD4i7t/svhM%3D">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Daniel Wieczorek</strong> <small>222606</small>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+      </p>
+    </div>
+</article>
+
+
+<article class="media">
+
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://openailabsprodscus.blob.core.windows.net/private/user-N3lhCunQtshrWgsB36Z8Cmi5/generations/generation-5ZkCq2byriA4c82ou7oiYDTb/image.webp?st=2022-09-16T20%3A58%3A17Z&se=2022-09-16T22%3A56%3A17Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2022-09-16T18%3A37%3A05Z&ske=2022-09-23T18%3A37%3A05Z&sks=b&skv=2021-08-06&sig=nJIlQ3zH%2BwI7TtqOMY6qKTmRTvqHkUU/mydWFfL8bKo%3D">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Adam Tadzik</strong> <small>222606</small>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+      </p>
+    </div>
+</article>
